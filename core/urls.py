66228724e0b9ee6cc2dedcc6618cbb96a7d12aaf
Jorge Romero C <jromerocontreras@icloud.com>
2023-08-16 09:34:35 -0400

    Aplicacion contact  es independiente del sistema

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import home, contact
+from .views import home
 
 urlpatterns = [
     path('', home, name='home'),
-    path('contact/', contact, name='contact'),
 ]
